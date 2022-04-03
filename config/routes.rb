cbd470a81a47431c415cdf085b5d5c99dff5c395
Shaun Limbeek <slimbeek@shauns-mbp.lan>
2022-04-03 11:32:02 -0700

    Create the dashboard page and links to it

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :epxenses
   resources :roommates
   devise_for :homes
+
+  get 'home/dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
   # Defines the root path route ("/")
