79bab0ab8b07951c35b03a2350bbd4899ff93d4e
Shaun Limbeek <slimbeek@shauns-mbp.lan>
2022-04-07 12:58:31 -0700

    Updated controllers to link chores to the calendar page and all sub-models to the dashboard page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
   # GET /events or /events.json
   def index
     @events = Event.all
+    @chores = Chore.all
   end
 
   # GET /events/1 or /events/1.json
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def dashboard
+    @roommates = Roommate.all
+    @events = Event.all
+    @choress = Chore.all
+    @expenses = Epxense.all
+  end
 end
